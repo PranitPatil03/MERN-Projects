e524feeb616121e938f6eef7538344c5cd089202
pranit <patilpranit3112@gmail.com>
2022-08-09 13:11:27 +0530

    Database connected

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const  mongoURL="mongodb://localhost:27017/"
 
 connectToDadatabase=()=>{
-    mongoose.connect(mongoURL,()=>{console.log('connected to Database')});
+    mongoose.connect(mongoURL,()=>{
+            console.log('connected to Database')
+    });
 }
 
 module.exports=connectToDadatabase
