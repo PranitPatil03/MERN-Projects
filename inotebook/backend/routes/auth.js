354d4ea3f40ab9be1c4420f5918821b1229e5d5d
pranit <patilpranit3112@gmail.com>
2022-08-10 18:24:04 +0530

    Create Collections

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -4,22 +4,36 @@ const { body, validationResult } = require('express-validator');
 const router = express.Router()
 
 
-router.post('/',[
+router.post('/createUser',[
     body('email').isEmail(),
     body('name').isLength({ min: 3 }),
     body('password').isLength({ min: 5 })
-],(req, res) => {
+], async (req, res) => {
     
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-      }).then(user => res.json(user));
+
+    try {
+      let user=await User.findOne({email:req.body.email})
+
+      if(user){
+        return res.status(400).json({errors: "Email already registered"})
+      }
+  
+        user=await User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password,
+        })
+  
+        res.json(user) 
+    } catch (error) {
+      console.error(error.message)
+      res.status(500).send("Some Error Occecued")
+    }
 
 })
 
