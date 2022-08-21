446b370a2add7372564fff541881f2b0c73c1748
pranit <patilpranit3112@gmail.com>
2022-08-21 12:00:24 +0530

    Signup component error

--- inotebook/frontend/notebook/src/components/Signup.js
+++ inotebook/frontend/notebook/src/components/Signup.js
@@ -1,8 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'; // version 5.2.0
+
 
 function Signup() {
+  const navigate = useNavigate()
+  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" })
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const {name, email, password} = credentials
+    const response = await fetch("http://localhost:8000/api/auth/createuser", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, password })
+    });
+    const json = await response.json()
+    console.log(json);
+    if(json.success){
+      localStorage.setItem('token', json.authtoken)
+      navigate('/')
+    }else{
+      alert("Invaild Credentials")
+    }
+  }
+
+  const onChange = (e) => {
+    setCredentials({ ...credentials, [e.target.name]: e.target.value })
+  }
+
   return (
-    <div>Signup</div>
+    <div className="container mx-2">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">Name</label>
+          <input type="text" className="form-control" onChange={onChange} value={credentials.name} id="name" name="name" aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">Email address</label>
+          <input type="email" className="form-control" onChange={onChange} value={credentials.email}  id="email" name="email" aria-describedby="emailHelp" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">Password</label>
+          <input type="password" className="form-control" onChange={onChange} value={credentials.password} name="password" id="password" />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label">Confirm Password</label>
+          <input type="password" className="form-control" onChange={onChange} value={credentials.cpassword} name="cpassword" id="cpassword" />
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
   )
 }
 
